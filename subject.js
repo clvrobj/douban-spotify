415dd42e559bb93d8394920e8281c304578f7bf8
Zhang <clvrobj@gmail.com>
2012-07-23 00:02:25 +0800

    subject page

--- subject.js
+++ subject.js
@@ -1,10 +1,13 @@
-var qpath = 'http://ws.spotify.com/search/1/album.json', album = $('h1 span').text();
+var qpath = 'http://ws.spotify.com/search/1/album.json', album = $('h1 span').text(),
+isOpenSpotifyDirect = null;
 
 var showAlbumsMenu = function (albums) {
     $('<h2><span class="spotify-btn"></span>'.concat(album, ' on Spotify</h2><div id="spotify-albums"></div>')).prependTo('.aside');
     var albumsMenu = $('#spotify-albums');
     $.each(albums, function (idx, album) {
-               $('<div class="spotify-album"><a href="'.concat(album.href, '">', album.name, ' - ', album.artists[0].name, '</a></div>'))
+               var link = isOpenSpotifyDirect ? album.href : 'http://open.spotify.com/album/'.concat(album.href.split(':')[2]),
+               target = isOpenSpotifyDirect ? '' : '_blank';
+               $('<div class="spotify-album"><a href="'.concat(link, '" target="', target, '">', album.name, ' - ', album.artists[0].name, '</a></div>'))
                    .appendTo(albumsMenu).hover(
                        function () {
                            $(this).addClass('mover');
@@ -15,13 +18,17 @@ var showAlbumsMenu = function (albums) {
     return albumsMenu;
 };
 
-$.ajax({url:qpath, 
-        crossDomain:true,
-        data:{q:album},
-        success:function (ret) {
-            if (ret.info.num_results && ret.info.num_results > 0) {
-                var q = ret.info.query;
-                showAlbumsMenu(ret.albums);
-            }
-        }
-       });
+chrome.extension.sendRequest({method:"getLocalStorage", key:"isOpenSpotifyDirect"},
+                             function(response) {
+                                 isOpenSpotifyDirect = (response.data == 'false') ? false : true;
+                                 $.ajax({url:qpath, 
+                                         crossDomain:true,
+                                         data:{q:album},
+                                         success:function (ret) {
+                                             if (ret.info.num_results && ret.info.num_results > 0) {
+                                                 var q = ret.info.query;
+                                                 showAlbumsMenu(ret.albums);
+                                             }
+                                         }
+                                        });
+                             });
